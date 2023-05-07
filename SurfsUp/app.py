3291796a4f3be5f3fa542286064573cbf42b60b8
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2023-05-07 17:43:12 -0400

    added some queries

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,27 +1,31 @@
 # Import the dependencies.
-
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+print(Base.classes.keys())
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
